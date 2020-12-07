3f72c2dff0c03bf910f95d409509c1679132360e
BenGumaan <ben.gumaan@gmail.com>
2020-12-07 09:34:51 +0300

    First version

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+// javascript Animate onscroll Start
+// $(document).ready(function () {
+//     if (screen.width > 1024) {
+//         AOS.init({
+//             easing: 'ease-in-out-sine',
+//             once: true,
+//         });
+//     }
+// });
+// window.onscroll = () => { scrollFunction() };
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// Hide the dropdown menu after clicking on one of the menu items
+$(function () {
+    var navMain = $(".navbar-collapse");
+
+    navMain.on("click", "a", null, function () {
+        navMain.collapse('hide');
+    });
+});
+
+//nav
+$(window).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+    scrollFunction()
+});
+
+// $(document).ready(function () {
+//     $(document).on("scroll", onScroll);
+
+//     //smoothscroll
+//     $('a[href^="#"]').on('click', function (e) {
+//         e.preventDefault();
+//         $(document).off("scroll");
+
+//         $('a').each(function () {
+//             $(this).removeClass('active');
+//         })
+//         $(this).addClass('active');
+
+//         var target = this.hash,
+//             menu = target;
+//         $target = $(target);
+//         $('html, body').stop().animate({
+//             'scrollTop': $target.offset().top + 2
+//         }, 500, 'swing', function () {
+//             window.location.hash = target;
+//             $(document).on("scroll", onScroll);
+//         });
+//     });
+// });
